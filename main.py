5556324c02273058c8c2bce2c7a53acc834d455d
midspooj <142264378+midspooj@users.noreply.github.com>
2023-09-10 18:14:44 -0400

    Update main.py with the function analyze_world_indicators

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import seaborn.objects as so
+import seaborn as sns
+from matplotlib import style
 
-def IntFactorial(x):
-    """This is a recursive function
-    to find the factorial of an integer"""
-    if x == 1:
-        return 1
-    else:
-        # recursive call to the function
-        return (x * IntFactorial(x-1))
+def analyze_world_indicators(csv_url):
+    # Download World Development Indicators
+    wdi = pd.read_csv(csv_url)
+
+    # GDP Per Capita has a REALLY long right tail, so we want to log it for readability.
+    wdi["Log GDP Per Capita"] = np.log(wdi["GDP per capita (constant 2010 US$)"])
+
+    # Plot
+    my_chart = (
+        so.Plot(
+            wdi, x="Log GDP Per Capita", y="Mortality rate, under-5 (per 1,000 live births)"
+        )
+        .add(so.Line(), so.PolyFit(order=2))
+        .add(so.Dot())
+        .label(title="Log GDP and Under-5 Mortality")
+        .theme({**style.library["seaborn-whitegrid"]})
+    )
+
+    return wdi, my_chart
+
+# Example usage
+csv_url = "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+data, chart = analyze_world_indicators(csv_url)
+
+# Print some statistical data
+print(data.describe())
+
