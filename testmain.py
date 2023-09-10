46388b952332b94c249fcc38bdd8dbb20c35bf6a
midspooj <142264378+midspooj@users.noreply.github.com>
2023-09-10 19:00:56 -0400

    Update testmain.py to include the test for analyze_world_indicators

--- testmain.py
+++ testmain.py
@@ -5,10 +5,35 @@ TESTS goes here
 """
 
 
-from main import IntFactorial
+from main import analyze_world_indicators
 
-def test_main():
-    print("The factorial of 4 is :", IntFactorial(4))
-    assert IntFactorial(4) == 24
 
-test_main()
+def test_analyze_world_indicators():
+    # Define the CSV URL for testing
+    test_csv_url = "https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+
+    import pandas as pd
+    import numpy as np
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    # Call the function and capture the returned values
+    data, chart = analyze_world_indicators(test_csv_url)
+
+    
+    
+
+
+    # Check if data is a DataFrame
+    assert isinstance(data, pd.DataFrame), "data should be a pandas DataFrame"
+
+    # Check if chart is a Seaborn plot object
+    assert isinstance(chart, so.Plot), "chart should be a Seaborn plot object"
+
+    # Check if the data DataFrame has been modified correctly
+    assert "Log GDP Per Capita" in data.columns, "Log GDP Per Capita column missing"
+
+    # Add more assertions if needed based on your specific requirements
+
+# Run the testing function
+test_analyze_world_indicators()
